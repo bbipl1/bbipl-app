de456010d2ce738f5dd28d0b8115f39717f52007
Your Name <you@example.com>
2025-02-13 01:29:19 +0530

    13-02-2025(Thursday)

--- src/pages/officials/constructions/siteEngineer/HDDForms.jsx
+++ src/pages/officials/constructions/siteEngineer/HDDForms.jsx
@@ -12,7 +12,7 @@ const HDDForms = ({ siteEngineerId }) => {
   const [dia, setDia] = useState();
   const [NoOfJobs, setNoOfJobs] = useState();
   const [fuel, setFuel] = useState([]);
-  const [km, setkm] = useState();
+  const [meter, setMeter] = useState();
   const [rate, setRate] = useState();
   const [amount, setAmount] = useState();
   const [expenses, setExpenses] = useState([]);
@@ -32,7 +32,7 @@ const HDDForms = ({ siteEngineerId }) => {
       dateOfRequirements,
       dia,
       NoOfJobs,
-      km,
+      meter,
       rate,
       amount,
       expenses,
@@ -57,11 +57,11 @@ const HDDForms = ({ siteEngineerId }) => {
     if (!NoOfJobs) {
       alert("No Of Jobs are required.");
     }
-    if (!km) {
-      alert("kilometer is required.");
+    if (!meter) {
+      alert("meter is required.");
     }
     if (!rate) {
-      alert("raete is required.");
+      alert("rate is required.");
     }
     if (!paymentRec) {
       alert("payment status is required.");
@@ -113,8 +113,8 @@ const HDDForms = ({ siteEngineerId }) => {
 
   const handleFuel = (e) => {
     // const [name,value]=e.target;
-    if(!e.target.value){
-      return ;
+    if (!e.target.value) {
+      return;
     }
     setFuel((f) => {
       // Check if the value already exists in the array
@@ -181,16 +181,16 @@ const HDDForms = ({ siteEngineerId }) => {
             className="w-full border-2 p-2"
           >
             <option value="">Select</option>
-            <option value="">63</option>
-            <option value="">75</option>
-            <option value="">90</option>
-            <option value="">110</option>
-            <option value="">125</option>
-            <option value="">140</option>
-            <option value="">160</option>
-            <option value="">180</option>
-            <option value="">200</option>
-            <option value="">220</option>
+            <option value="63">63</option>
+            <option value="75">75</option>
+            <option value="90">90</option>
+            <option value="110">110</option>
+            <option value="125">125</option>
+            <option value="140">140</option>
+            <option value="160">160</option>
+            <option value="180">180</option>
+            <option value="200">200</option>
+            <option value="220">220</option>
           </select>
         </div>
         <div>
@@ -204,102 +204,59 @@ const HDDForms = ({ siteEngineerId }) => {
             className="w-full border-2  p-2"
           />
         </div>
-        <div>
-          <label className="" htmlFor="">
-            Vehicles used details
-          </label>
+       
+
           <div>
-            <label htmlFor="fuel">Fuel</label>
-            <select
-              value={fuel[fuel.length]?.fuel}
+            <label className="pl-2" htmlFor="">
+              Meter
+            </label>
+            <input
+              className="w-full p-1"
+              value={meter}
+              placeholder="KiloMeter"
+              name="km"
+              id="km"
+              type="number"
               onChange={(e) => {
-                handleFuel(e);
+                setMeter(e.target.value);
               }}
-              name="fuel"
-              className="w-full p-2"
-              id="fuel"
-            >
-              <option value="">Select</option>
-              <option value="Petrol">Petrol</option>
-              <option value="Deisel">Deisel</option>
-              <option value="CNG">CNG</option>
-              {/* <option value="">Others</option> */}
-            </select>
-
-            {fuel &&
-              fuel.map((f) => {
-                return (
-                  <>
-                    <div className="w-full flex justify-between border-2 border-gray-100 p-1 bg-gray-50">
-                      <h1>{f.fuel}</h1>
-                      <button
-                        onClick={() => {
-                          handleRemoveFuel(f.fuel);
-                        }}
-                        className="bg-red-500 hover:bg-red-600  px-2 rounded-lg text-white"
-                      >
-                        Delete
-                      </button>
-                    </div>
-                  </>
-                );
-              })}
+            />
           </div>
-          {fuel && (fuel?.length>0) && (
-            <div className="grid grid-cols-1 md:grid-cols-1  lg:grid-cols-3  gap-4 ">
-              <div>
-                <label className="pl-2" htmlFor="">
-                  KiloMeter
-                </label>
-                <input
-                  className="w-full p-1"
-                  value={km}
-                  placeholder="KiloMeter"
-                  name="km"
-                  id="km"
-                  type="number"
-                  onChange={(e) => {
-                    setkm(e.target.value);
-                  }}
-                />
-              </div>
 
-              <div>
-                <label className="pl-2" htmlFor="Rate">
-                  Rate (fuel) /ltr
-                </label>
-                <input
-                  placeholder="Rate/ltr"
-                  type="number"
-                  className="w-full p-1"
-                  value={rate}
-                  name="rate"
-                  id="rate"
-                  onChange={(e) => {
-                    setRate(e.target.value);
-                  }}
-                />
-              </div>
-              <div>
-                <label className="pl-2" htmlFor="">
-                  Amount (INR)
-                </label>
-                <input
-                  placeholder="Amount"
-                  className="w-full p-1"
-                  type="number"
-                  value={amount}
-                  name="amount"
-                  id="amount"
-                  onChange={(e) => {
-                    setAmount(e.target.value);
-                  }}
-                />
-              </div>
-            </div>
-          )}
+          <div>
+            <label className="pl-2" htmlFor="Rate">
+              Rate/meter
+            </label>
+            <input
+              placeholder="Rate/meter"
+              type="number"
+              className="w-full p-1"
+              value={rate}
+              name="rate"
+              id="rate"
+              onChange={(e) => {
+                setRate(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label className="pl-2" htmlFor="">
+              Amount (INR)
+            </label>
+            <input
+              placeholder="Amount"
+              className="w-full p-1"
+              type="number"
+              value={amount}
+              name="amount"
+              id="amount"
+              onChange={(e) => {
+                setAmount(e.target.value);
+              }}
+            />
+          
         </div>
-        <div>
+        <div className="">
           <label htmlFor="">Payment received from client*</label>
           <div className="grid grid-cols-5 gap-4 relative">
             <div>
@@ -335,7 +292,7 @@ const HDDForms = ({ siteEngineerId }) => {
           </div>
           {paymentRec === "Yes" && (
             <>
-              <div className="absolute z-20 bg-neutral-200 w-5/6 p-2">
+              <div className="border-2 border-neutral-100 w-full p-2">
                 <label htmlFor="">Amount*</label>
                 <input
                   onChange={(e) => {
@@ -378,8 +335,8 @@ const HDDForms = ({ siteEngineerId }) => {
             <option name="advance" value="advance">
               Advance
             </option>
-            <option name="labourExpense" value="labourExpense">
-              Labour expense
+            <option name="labourExpenses" value="labourExpenses">
+              Labour expenses
             </option>
             <option name="others" value="others">
               Others
@@ -391,7 +348,13 @@ const HDDForms = ({ siteEngineerId }) => {
               return (
                 <>
                   <div className="w-full flex justify-between">
-                    <h1>{expense.expenses} <input type="number" className="border-2 border-blue-50 p-1" /></h1>
+                    <h1>
+                      {expense.expenses}{" "}
+                      <input
+                        type="number"
+                        className="border-2 border-blue-50 p-1"
+                      />
+                    </h1>
                     <button
                       onClick={() => {
                         handleRemove(expense.expenses);
